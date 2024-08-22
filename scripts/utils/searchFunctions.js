c4a5e4c0b25abfe1c14e0ffda0f5d96e21a989ca
gandrica <gandrica1983@gmail.com>
2024-08-22 12:06:26 +0200

    Suppression du code commenté

--- scripts/utils/searchFunctions.js
+++ scripts/utils/searchFunctions.js
@@ -4,16 +4,6 @@ import { arrayToLowerCase } from "./stringFunctions.js";
 
 //L'algorithme de recherche pour la recherche principale
 export function searchText(arr,text){
-    /* const arrFiltered = [];
-
-    for(let i = 0; i<arr.length;i++){
-        if(arr[i].name.toLowerCase().includes(text.toLowerCase()) 
-            || arrayToLowerCase(arr[i].ingredients).includes(text.toLowerCase())
-            || arr[i].description.toLowerCase().includes(text.toLowerCase())){
-                arrFiltered.push(arr[i]);
-            }
-    } */
-
     //Vérifie si le nom, les ingredients ou la description d'une recette, contiennent le texte passé en arguments - tout en minuscules
     let arrFiltered = arr.filter((recipe)=>{
             return recipe.name.toLowerCase().includes(text.toLowerCase()) 
