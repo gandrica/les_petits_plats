c74d40db4bb9934583cab6785afac8806d579020
gandrica <gandrica1983@gmail.com>
2024-08-28 11:13:14 +0200

    Création de la branch second avec la version de la boucle for

--- scripts/utils/searchFunctions.js
+++ scripts/utils/searchFunctions.js
@@ -4,11 +4,17 @@ import { arrayToLowerCase } from "./stringFunctions.js";
 
 //L'algorithme de recherche pour la recherche principale
 export function searchText(arr,text){
+    const arrFiltered = [];
+    
     //Vérifie si le nom, les ingredients ou la description d'une recette, contiennent le texte passé en arguments - tout en minuscules
-    let arrFiltered = arr.filter((recipe)=>{
-            return recipe.name.toLowerCase().includes(text.toLowerCase()) 
-            || arrayToLowerCase(recipe.ingredients).includes(text.toLowerCase())
-            || recipe.description.toLowerCase().includes(text.toLowerCase())
-        });
+    for(let i = 0; i<arr.length;i++){
+        if(arr[i].name.toLowerCase().includes(text.toLowerCase()) 
+            || arrayToLowerCase(arr[i].ingredients).includes(text.toLowerCase())
+            || arr[i].description.toLowerCase().includes(text.toLowerCase())){
+                arrFiltered.push(arr[i]);
+            }
+    }
+
     return arrFiltered;
+
 }
